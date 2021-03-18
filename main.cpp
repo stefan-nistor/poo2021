1d8979fa47a6070b5bdba97dd2be50af43e91eb9
Stefan Nistor <stefannistor10@gmail.com>
2021-03-18 07:25:41 +0200

    Finished Number class

--- main.cpp
+++ main.cpp
@@ -7,13 +7,31 @@ int main()
     Number n2("734",8);
     Number n3("FF",16);
 
-    Number n = n1;
-    //n.switchBase(4);
-    std::cout << n.number << '\n';
-    n.print();
-    n--;
-    n.print();
-    std::cout << n.number;
+    printf("n1 has %d digits and it is written in base %d\n",n1.getDigitsCount(),n1.getBase());
+
+    for (int tr=0;tr<n1.getDigitsCount();tr++)
+        printf("n1[%d]=%c\n",tr,n1[tr]);
+
+    n1.print();
+    n2.print();
+    n1 = (n2+n3-n1)+n1; // after this n1 will be in base 16
+    n1.switchBase(2);
+    n1.print();
+
+    if (n1>n2) printf("n1 is bigger than n2\n"); else printf("n2 is bigger than n1\n");
+
+    Number n4 = 12345; // n4 will be in base 10
+
+    n1 = 255; // n1 will be 11111111 (value 255 from base 10 in base 2)
+    n4 += n1;
+
+    n4.print();
+    n4 = "13579"; // n4 mentains its base (10) and will be 13579
+    n4.print();
+    --n4; // the first digit from n4 will be remove ==> n4 becomes 3579
+    n4.print();
+    n4--; // the last digit from n4 will be remove ==> n4 becomes 357
+    n4.print();
 
     return 0;
 
