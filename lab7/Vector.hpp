a58233b2977a6caf5d8643c80c7d01099e35c0a9
Stefan Nistor <stefannistor10@gmail.com>
2021-04-01 09:09:59 +0300

    Lab 7 ex 1 + ex 2

--- lab7/Vector.hpp
+++ lab7/Vector.hpp
@@ -12,6 +12,7 @@ typedef uint64 Size;
 #include <utility>
 #include <algorithm>
 #include <exception>
+#include <cstring>
 #include <iostream>
 
 template <class T>
@@ -25,7 +26,7 @@ private:
     using Value = T;
     using ValueReference = T &;
     using ValueConstReference = T const &;
-    using Adrerss = T *;
+    using Address = T *;
 
     auto expandWith ( Size ) noexcept -> void;
     auto _resize    ( Size ) noexcept -> void;
@@ -42,32 +43,22 @@ public:
     constexpr auto pop  () const noexcept -> Value { return this->_pData[_size];}
 
     auto remove         ( Index ) noexcept(false) -> void;
-   // auto insert         ( ValueConstReference, Index ) noexcept(false) -> void;
     auto insert         ( Value, Index ) noexcept(false) -> void;
 
     auto get            ( Index ) const noexcept (false) -> ValueConstReference;
     auto set            ( ValueConstReference, Index ) noexcept(false) -> ValueConstReference;
 
-    constexpr auto count () const noexcept -> uint64 { return this->_size; }
-    constexpr auto size  () const noexcept -> uint64 { return this->_size; }
+    constexpr auto count () const noexcept -> Size { return this->_size; }
+    constexpr auto size  () const noexcept -> Size { return this->_size; }
     constexpr auto print () const noexcept -> void;
 
-    auto sort           (bool (* sortFunc) (ValueConstReference, ValueConstReference) noexcept) noexcept -> void;
+    auto sort           (int (* sortFunc) (ValueConstReference, ValueConstReference) noexcept = nullptr ) noexcept -> void;
 
-    auto firstIndexOf   ( ValueConstReference obj, auto(* callbackFunction) (ValueConstReference, ValueConstReference) noexcept -> bool = nullptr) const noexcept -> Index{
+#pragma clang diagnostic push
+#pragma ide diagnostic ignored "NotImplementedFunctions"
+    auto firstIndexOf   ( ValueConstReference obj, auto(* callbackFunction) (ValueConstReference, ValueConstReference) noexcept -> bool = nullptr) const noexcept-> Index;
+#pragma clang diagnostic pop
 
-        if ( callbackFunction == nullptr) {
-            for (Index i = 0; i < this->_size; i++)
-                if (obj[i] == this->_pData[i])
-                    return i;
-        }
-        else {
-            for (Index i = 0; i < this->_size; i++)
-                if (callbackFunction(obj[i], _pData[i]))
-                    return i;
-        }
-        return -1;
-    }
 };
 
 template<class T>
@@ -94,7 +85,7 @@ auto Vector<T>::push(Value value) noexcept -> void {
 template<class T>
 auto Vector<T>::_resize(Size size) noexcept -> void {
 
-    auto newMemory = new Adrerss [size];
+    auto newMemory = new Address [size];
 
     for( Index i = 0; i < size; i++){
         if ( i < this->_capacity)
@@ -121,36 +112,44 @@ auto Vector<T>::expandWith( Size requiredSize) noexcept -> void {
 template<class T>
 auto Vector<T>::remove(Index index) noexcept(false) -> void {
     if(index > this->_size)
-        throw "Index out of bounds.";
+        throw std::runtime_error("Index out of bounds.");
 
-    Vector without;
+    auto without = new Address[this->_capacity];
+    memset(without, 0, sizeof(std::size_t) * this->_capacity);
 
     for(Index i = 0, j = 0; i < this->_size; i++) {
         if (i != index)
-            without._pData[j] = this->_pData[j], j++;
+            without[j++] = this->_pData[i];
     }
     this->_pData = without;
-    _size--;
+    this->_size--;
 }
 
 template<class T>
 auto Vector<T>::insert(Value obj, Index index) noexcept(false) -> void {
     if (index > this->_size)
-        throw "Index out of bounds.";
+        throw std::runtime_error("Out of bounds");
+    else if ( index == this->_size && index == this->_capacity )
+        this->_resize(this->_size + 1);
 
-    Vector with;
-    with._capacity = this->_capacity;
+    auto with = new Address[this->_capacity];
+    memset(with, 0, sizeof(std::size_t) * this->_capacity);
+
     for(Index i = 0; i < index; i++)
-        with._pData[i] = this ->_pData[i];
+        with[i] = this->_pData[i];
 
-    with._pData[index] = &obj;
+    with[index] = new T(obj);
 
     for(Index i = index + 1; i < this->_size + 1; i++)
-        with._pData[i] = this->_pData[i];
+        with[i] = this->_pData[i - 1];
 
-    this->_pData = with._pData;
+    for(Index i = this->_size + 1; i < this->_capacity - 1; i++)
+        with[i + 1] = this->_pData[i];
+
+    delete [] this->_pData;
+
+    this->_pData = with;
     this->_size++;
-
 }
 
 template<class T>
@@ -171,24 +170,51 @@ constexpr auto Vector<T>::print() const noexcept -> void {
 template<class T>
 auto Vector<T>::get(Index index) const noexcept(false) -> ValueConstReference {
     if (index < 0 || index > this->_size)
-        throw "Index out of bounds.";
+        throw std::runtime_error("Index out of bounds.");
     return * this->_pData[index];
 }
 
 template<class T>
 auto Vector<T>::set(ValueConstReference value, Index index) noexcept(false) -> ValueConstReference {
     if (index < 0 || index > this->_size)
-        throw "Index out of bounds.";
+        throw std::runtime_error("Index out of bounds.");
     return *this->_pData[index] = value;
 }
 
-//TODO: implement sort function
 template<class T>
-auto Vector<T>::sort(bool (*sortFunc)(ValueConstReference, ValueConstReference) noexcept) noexcept -> void {
-    if (sortFunc == nullptr) {
-        return;
-    }
+auto Vector<T>::firstIndexOf(ValueConstReference obj, auto (*callbackFunction)(ValueConstReference, ValueConstReference) noexcept -> bool) const noexcept -> Index {
 
+    if ( callbackFunction == nullptr) {
+        for (Index i = 0; i < this->_size; i++)
+            if (obj == *this->_pData[i])
+                return i;
+    }
+    else {
+        for (Index i = 0; i < this->_size; i++)
+            if (callbackFunction(obj, *_pData[i]))
+                return i;
+    }
+    return -1;
+}
+
+
+template<class T>
+auto Vector<T>::sort(int (*sortFunc)(ValueConstReference , ValueConstReference ) noexcept) noexcept -> void {
+    for (Index i = 0; i < this->_size - 1; i++) {
+        for (Index j = 0; j < this->_size - 1 - i; j++) {
+            if (sortFunc != nullptr) {
+                if (sortFunc(*this->_pData[j], *this->_pData[j + 1])) {
+                    Address aux = this->_pData[j];
+                    this->_pData[j] = this->_pData[j + 1];
+                    this->_pData[j + 1] = aux;
+                }
+            } else if (this->_pData[j] < this->_pData[j + 1]) {
+                Address aux = this->_pData[j];
+                this->_pData[j] = this->_pData[j + 1];
+                this->_pData[j + 1] = aux;
+            }
+        }
+    }
 }
 
 #endif //LAB1_VECTOR_HPP
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include  <iostream>
-#include "lab7/Vector.hpp"
+
 float operator"" _Kelvin(const char *temperature){
     return float(strtod(temperature, nullptr)) - 273.0f;
 }
@@ -10,25 +10,10 @@ float operator"" _Fahrenheit (const char* temperature){
 
 }
 
-Vector<int> v;
-
 int main()
 {
-
-//    float a = 300_Kelvin;
-//    float b = 120_Fahrenheit;
-//    std::cout << a << ' ' << b;
-
-    v.push(1);
-    v.push(2);
-    v.push(5);
-    v.push(6);
-    v.push(7);
-    v.print();
-
-    v.insert(23, 2);
-
-//    v.print();
+    float a = 300_Kelvin;
+    float b = 120_Fahrenheit;
+    std::cout << a << ' ' << b;
     return 0;
-
 }
